176f2661e4cad039280cde5465ce6892d75fc4d1
adeministratorTr <adembavas@gmail.com>
2021-05-05 20:09:45 +0300

    Move Header into components & Update style

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+function Header() {
+  return (
+    <header className="app-header">
+      <p>Welcome to the Line Chart Project :)</p>
+    </header>
+  )
+}
+
+export default Header

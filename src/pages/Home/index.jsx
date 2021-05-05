136f11176a1fd5fc5dc2b1a1875589a0a3083b9e
adeministratorTr <adembavas@gmail.com>
2021-05-05 21:32:22 +0300

    Add Top stories request to home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,38 @@
+import { useState } from 'react'
+import { useQuery } from 'react-query'
 import { Button, TextField } from '@material-ui/core'
+
+import { getTopStoryList } from 'services/story'
 
 import './style.scss'
 
 function Home() {
+  const [storyLimit, setStoryLimit] = useState(process.env.REACT_APP_DEFAULT_CHART_COUNT)
+
+  const storyList = useQuery('getTopStoryList', getTopStoryList, {
+    enabled: false,
+    onSuccess: (data) => {
+      data.length = storyLimit
+      console.log(data) // TODO map story data
+    }
+  })
+
   return (
     <div className='home-page'>
       <div className='user-inputs'>
         <TextField
-          className='limit-input'
           label='Number of Story'
           type='number'
           variant='outlined'
           helperText='Max 50'
+          value={storyLimit}
+          onChange={(e) => setStoryLimit(e.target.value)}
         />
         <Button
           className='limit-button'
           variant='contained'
           color='primary'
+          onClick={() => storyList.refetch()}
         >
           Show Stories Chart
         </Button>

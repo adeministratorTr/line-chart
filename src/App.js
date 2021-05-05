4a271315fa5bfe4db6107c7bbe201b05a10c62f8
adeministratorTr <adembavas@gmail.com>
2021-05-05 20:15:36 +0300

    Add home page to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { Suspense, lazy } from 'react';
+
 import Header from 'components/Header'
+import Loading from 'components/Loading'
 
 import './App.css'
+
+const Home = lazy(() => import('pages/Home'));
 
 function App() {
   return(
     <div className="app">
       <Header />
+      <Suspense fallback={<Loading />}>
+        <Home />
+      </Suspense>
     </div>
   )
 }

fceb6974cd3f2fb15036a3af737682d916fbe5db
adeministratorTr <adembavas@gmail.com>
2021-05-05 21:38:03 +0300

    Add Story Item service

--- src/services/story.js
+++ src/services/story.js
@@ -5,3 +5,8 @@ export const getTopStoryList = () =>
   fetch(`${getApiUrl()}/topstories.json`)
     .then((resp) => resp.json())
     .then((result) => result)
+
+export const getStoryItem = (itemId) =>
+  fetch(`${getApiUrl()}/item/${itemId}.json`)
+    .then((resp) => resp.json())
+    .then((result) => result)
